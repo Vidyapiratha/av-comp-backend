9d2009316c80da8183bee4cf44f61b6338e4a7e6
Asif Sorowar <39998973+asifsorowar@users.noreply.github.com>
2024-03-14 19:06:57 +0600

    checkPortalUserAbility functionality added

--- compapi/clientManagementApi/src/queries/portalUser.js
+++ compapi/clientManagementApi/src/queries/portalUser.js
@@ -1,14 +1,14 @@
 const { pgDbPromise } = require("../database/connection");
 
 const getPortalUserById = async ({ id }) => {
-  let queryText = `SELECT p_u.*, p_r.role_name FROM portal_users as p_u
+  let queryText = `SELECT p_u.*, p_r.role_name FROM portal_users as p_u 
         JOIN portal_user_roles as p_r
             ON p_u.user_role_id = p_r.id
         JOIN portal_clients as p_c
           On p_c.id = p_u.client_id`;
 
   queryText += ` WHERE p_u.id = $1 and 
-    NOT p_u.deleted AND p_u.is_active AND NOT p_c.deleted AND p_c.enabled limit 1`;
+    p_u.is_active AND p_c.enabled limit 1`;
 
   const query = {
     text: queryText,
@@ -33,6 +33,32 @@ const getPortalUserById = async ({ id }) => {
   }
 };
 
+const getPortalUserIdByEmail = async (email) => {
+  const query = {
+    text: `SELECT id FROM portal_users
+        WHERE email = $1 limit 1`,
+    values: [email],
+  };
+
+  try {
+    const pdDb = await pgDbPromise();
+
+    const data = await pdDb.query(query);
+    if (!data.length) {
+      console.log(`user not found with email: '${email}'`);
+
+      return null;
+    }
+
+    return data[0].id;
+  } catch (error) {
+    console.log("Error on getting user:::", error);
+
+    return null;
+  }
+};
+
 module.exports = {
   getPortalUserById,
+  getPortalUserIdByEmail,
 };
