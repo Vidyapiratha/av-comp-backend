35e5aefcd5c40edf5ee7c9ce6065c96ddbd7a89f
Asif Sorowar <39998973+asifsorowar@users.noreply.github.com>
2024-03-15 14:22:42 +0600

    create user to congito added

---
+++ compapi/clientManagementApi/src/queries/cognito.js
@@ -0,0 +1,73 @@
+const AWS = require("aws-sdk");
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+const generatePassword = () => {
+  return (
+    Math.random().toString(36).slice(2) +
+    Math.random().toString(36).toUpperCase().slice(2)
+  );
+};
+
+const createCognitoUser = async ({
+  first_name,
+  last_name,
+  email,
+  phone_number,
+  client_id,
+}) => {
+  let params = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Username: email,
+    DesiredDeliveryMediums: ["EMAIL"],
+    ForceAliasCreation: true,
+    TemporaryPassword: generatePassword(),
+    UserAttributes: [
+      {
+        Name: "email",
+        Value: email,
+      },
+      {
+        Name: "email_verified",
+        Value: "true",
+      },
+      {
+        Name: "given_name",
+        Value: first_name,
+      },
+      {
+        Name: "family_name",
+        Value: last_name,
+      },
+      {
+        Name: "phone_number",
+        Value: phone_number,
+      },
+      {
+        Name: "phone_number_verified",
+        Value: "true",
+      },
+      {
+        Name: "custom:client_id",
+        Value: client_id,
+      },
+    ],
+  };
+
+  try {
+    const data = await cognitoIdentityServiceProvider
+      .adminCreateUser(params)
+      .promise();
+
+    console.log("The created cognito user::", data);
+
+    return data.User;
+  } catch (error) {
+    console.log("Error on createCognitoUser::", error);
+
+    return null;
+  }
+};
+
+module.exports = {
+  createCognitoUser,
+};
