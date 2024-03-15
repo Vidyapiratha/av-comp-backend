9d6bfa60463cc6d7f0ffdf80a02c091099274be2
Asif Sorowar <39998973+asifsorowar@users.noreply.github.com>
2024-03-15 19:52:25 +0600

    auth fetching issue fixed

--- compapi/clientManagementApi/src/middlewares/auth.js
+++ compapi/clientManagementApi/src/middlewares/auth.js
@@ -3,7 +3,7 @@ const { getPortalUserById } = require("../queries/portalUser");
 const setAuth = async (userId) => {
   if (!userId) throw Error("Unauthorized!");
 
-  const user = await getPortalUserById(userId);
+  const user = await getPortalUserById({ id: userId });
   if (!user) throw Error("Unauthorized!");
 
   console.log("The auth user:::", user);
